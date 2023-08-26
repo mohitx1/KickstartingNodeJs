8bacec66e59d6be76a6df5e7ea23736d54fb3d01
mohitx1 <mohitrockx3@gmail.com>
2023-08-26 23:06:11 +0530

    writing&readingFromFile-8

--- app.js
+++ app.js
@@ -1,36 +1,43 @@
-const http=require('http');
-const myName='Mohit kr'
+const http = require('http');
+const fs = require('fs');
 
-const server=http.createServer((req,res)=>{
-    // console.log(req.url , req.method , req.headers)
-    const url=req.url;
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+  if (url === '/') {
+    fs.readFile("message.txt",{encoding: 'utf-8'},(err,messageData)=>{
+        if(err){
+            console.log(err)
+        }
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title><head>');
+        res.write('<body>'+messageData+'</body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    })
 
-    if(url==='/home'){
-        res.setHeader('Content-Type','text/html'
-        )
-        res.write(`<html>`)
-        res.write(`<head><title>My first page</title></head>`)
-        res.write(`<body><h1> Welcome home</h1></body>`)
-        res.write(`</html>`)
-        return res.end()
-    }else if(url==='/about'){
-        res.setHeader('Content-Type','text/html'
-        )
-        res.write(`<html>`)
-        res.write(`<head><title>My first page</title></head>`)
-        res.write(`<body><h1> Welcome to About Us page</h1></body>`)
-        res.write(`</html>`)
-        return res.end()
-    }else if(url==='/node'){
-        res.setHeader('Content-Type','text/html'
-        )
-        res.write(`<html>`)
-        res.write(`<head><title>My first page</title></head>`)
-        res.write(`<body><h1>Welcome to my Node Js project</h1></body>`)
-        res.write(`</html>`)
-        return res.end()
-    }
+  }
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message,(err)=>{
+        if(err){
+            console.log(err)
+        }
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
 
-})
+  }
+});
 
 server.listen(3000);
